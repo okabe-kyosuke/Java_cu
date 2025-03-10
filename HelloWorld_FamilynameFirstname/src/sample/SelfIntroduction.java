19d081e03c7c2a8f8a1602738faaabedeea26377
okabe <okabe@192.168.0.15>
2025-03-10 22:38:59 +0900

    課題提出　５－３　　新規作成

--- HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -3,22 +3,17 @@ package sample;
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
-		// 氏名
 		String name = "岡部　恭祐";
-		// 年齢
 		int age = 25;
-		// 趣味
-		String hobby = "映画鑑賞";
-		
-		
-		/*
-		 *  自己紹介文を出力する処理
-		 */
+		String hobby = "旅行";
 
+		
+		/*自己紹介を出力する処理*/
+		
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "歳です。");
 		System.out.println("趣味は" + hobby + "です。");
-
+		System.out.println();
 	}
 
 }
