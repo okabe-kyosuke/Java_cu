8f3edd6586a5385f9f291ae65fa24241797c93a4
okabe <okabe@192.168.0.15>
2025-03-19 21:41:24 +0900

    カリキュラムAー１　新規作成

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13_1.java
@@ -0,0 +1,22 @@
+package curriculum_A;
+
+public class Qes1_13_1 {
+	//バイト型変数として宣言
+	byte b;
+	//短整数型変数として宣言
+	short s;
+	//整数型変数として宣言
+	int i;
+	//長整数型変数として宣言
+	long l;
+	//単精度浮動小数点数型変数として宣言
+	float f;
+	//倍精度浮動小数点数型変数として宣言
+	double d;
+	//文字型変数として宣言
+	char c;
+	//文字列型変数として宣言
+	String str;
+	//ブーリアン型変数として宣言
+	boolean boo;
+}
