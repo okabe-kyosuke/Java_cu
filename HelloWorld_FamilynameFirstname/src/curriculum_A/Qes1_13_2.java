5db6e888358375f2836582eb8915910bc253864b
okabe <okabe@192.168.0.15>
2025-03-19 22:25:46 +0900

    カリキュラムA1_13_2

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13_2.java
@@ -0,0 +1,42 @@
+package curriculum_A;
+
+public class Qes1_13_2 {
+	public static void main(String[] args) {
+		//バイト型変数として宣言
+		byte b;
+		//短整数型変数として宣言
+		short s;
+		//整数型変数として宣言
+		int i;
+		//長整数型変数として宣言
+		long l;
+		//単精度浮動小数点数型変数として宣言
+		float f;
+		//倍精度浮動小数点数型変数として宣言
+		double d;
+		//文字型変数として宣言
+		char c;
+		//文字列型変数として宣言
+		String str;
+		//ブーリアン型変数として宣言
+		boolean boo;
+		//バイト型変数の初期化
+		byte b = 0;
+		//短整数型変数の初期化
+		short s = 0;
+		//整数型変数の初期化		
+		int i = 0;
+		//長整数型変数の初期化
+		long l = 0;
+		//単精度浮動小数点数型変数の初期化
+		float f = 0.0f;
+		//倍精度浮動小数点数型変数の初期化
+		double d = 0.0d;
+		//文字型変数の初期化
+		char c = '\u0000';
+		//文字列型変数の初期化
+		String str = null;
+		//ブーリアン型変数の初期化
+		boolean boo = false;
+	}
+}
