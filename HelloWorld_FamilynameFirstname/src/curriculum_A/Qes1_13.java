b7164b23dbbafba843cf7b6e3126e39d2d5443b8
okabe <okabe@192.168.0.15>
2025-04-23 01:02:33 +0900

    A-4 バグったので少し変わってます。

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,88 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//バイト型変数として宣言
+				byte b;
+				//短整数型変数として宣言
+				short s;
+				//整数型変数として宣言
+				int i;
+				//長整数型変数として宣言
+				long l;
+				//単精度浮動小数点数型変数として宣言
+				float f;
+				//倍精度浮動小数点数型変数として宣言
+				double d;
+				//文字型変数として宣言
+				char c;
+				//文字列型変数として宣言
+				String str;
+				//ブーリアン型変数として宣言
+				boolean boo;
+				//バイト型変数の初期化
+				b = 0;
+				//短整数型変数の初期化
+				s = 0;
+				//整数型変数の初期化		
+				i = 0;
+				//長整数型変数の初期化
+				l = 0;
+				//単精度浮動小数点数型変数の初期化
+				f = 0.0f;
+				//倍精度浮動小数点数型変数の初期化
+				d = 0.0d;
+				
+				//初期化をした変数に代入する
+				
+				b = 10;
+				
+				s = 100;
+				
+				i = 1000;
+				
+				l = 10000;
+				
+				f = 9.5f;
+				
+				d = 10.5;
+				
+				c = 'a';
+				
+				str = "ハロー";
+				
+				boo = true;
+				
+				
+				//上記の変数を利用して値を出していく
+				//A-4
+				
+				long result = b + s + i +l;
+				
+				System.out.println(result);
+				
+				double result1 = f + d;
+				
+				System.out.println(result1);
+				
+				String result2 = c + str + boo;
+				
+				System.out.println(result2);
+				
+				double result3 = b + s + i + l + f + d;
+				
+				System.out.println(result3);
+				
+				double result4 = d / s;
+				
+				System.out.println(result4);
+				
+				double result5 = b - s;
+				
+				System.out.println(result5); 
+				
+	}
+
+}
