5e58ed90e23fe04a0fb27f3d23344886e31a5c83
okabe <okabe@192.168.0.15>
2025-04-28 23:00:35 +0900

    カリキュラムA-6

--- HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
@@ -93,6 +93,28 @@ public class Qes1_13 {
 				System.out.println("ハローJAVA"+(num+num1));
 				
 				
+				//変数を変更しフォーマットのとおりに出力
+				//A-6
+				
+				String name = "山田太郎";
+				
+				int age = 18;
+				
+				double height = 170.5;
+				
+				double weight = 62.2;
+				
+				String favoriteFood = "寿司";
+				
+				System.out.println("初めまして"+name+"です");
+				
+				System.out.println("年齢は"+age+"歳です");
+				
+				System.out.println("身長は"+height+"cmです");
+				
+				System.out.println("体重は"+weight+"kgです");
+				
+				System.out.println("好きな食べ物は"+favoriteFood+"です");
 				
 				
 				
